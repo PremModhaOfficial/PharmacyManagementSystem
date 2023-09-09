bc34e1be48ff08a78255b68288cd3dd4a6a7df04
Prem <premmodhaofficial@gmail.com>
2023-09-09 14:16:28 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Welcome to Pharmacy Management System
+                
+                Please Enter Login Information.  
+                
+                """);
+    }
+}
